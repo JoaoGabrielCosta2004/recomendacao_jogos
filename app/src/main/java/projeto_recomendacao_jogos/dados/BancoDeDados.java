7b1b11f5d0f79e49fff166bb47e91267f3eb08f3
Walmir07 <walmir.lima@academico.ifpb.edu.br>
2025-02-17 12:02:51 -0300

    commit arrumada

--- app/src/main/java/projeto_recomendacao_jogos/dados/BancoDeDados.java
+++ app/src/main/java/projeto_recomendacao_jogos/dados/BancoDeDados.java
@@ -1,26 +1,21 @@
-package projeto_recomendacao_jogos.conexao_bd;
+package projeto_recomendacao_jogos.dados;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class BancoDeDados {
-    public static void main(String[] args) {
-        String url = "jdbc:postgresql://ep-damp-bread-a8c72fk0-pooler.eastus2.azure.neon.tech/neondb?user=neondb_owner&password=npg_lb7T4dUiREIe&sslmode=require";
-        String usuario = "neondb_owner";
-        String senha = "npg_lb7T4dUiREIe";
-
-        try {
-            // Estabelece a conexão com o banco de dados
-            Connection conexao = DriverManager.getConnection(url, usuario, senha);
-            System.out.println("Conexão estabelecida com sucesso!");
-
-            // Não esqueça de fechar a conexão após o uso
-            conexao.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+public abstract class BancoDeDados {
+    protected static String URL = "jdbc:postgresql://ep-damp-bread-a8c72fk0-pooler.eastus2.azure.neon.tech/neondb?user=neondb_owner&password=npg_lb7T4dUiREIe&sslmode=require";
+    protected static String USUARIO = "neondb_owner";
+    protected static String SENHA = "npg_lb7T4dUiREIe";
+    
+    public Connection acessarConexao() throws SQLException{
+        return DriverManager.getConnection(URL, URL, SENHA);
     }
 
+    public abstract Object chamar(Object obj);
+    public abstract void criar(Object obj);
+    public abstract void atualizar(Object obj);
+    public abstract void deletar(Object obj);
 
 }
--- app/src/main/java/projeto_recomendacao_jogos/dados/ManupularConteudos.java
+++ app/src/main/java/projeto_recomendacao_jogos/dados/ManupularConteudos.java
@@ -1,5 +1,31 @@
 package projeto_recomendacao_jogos.dados;
 
-public class ManupularConteudos {
+public class ManupularConteudos extends BancoDeDados{
+
+    @Override
+    public Object chamar(Object obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'chamar'");
+    }
+
+    @Override
+    public void criar(Object obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'criar'");
+    }
+
+    @Override
+    public void atualizar(Object obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'atualizar'");
+    }
+
+    @Override
+    public void deletar(Object obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deletar'");
+    }
+
+    
 
 }
