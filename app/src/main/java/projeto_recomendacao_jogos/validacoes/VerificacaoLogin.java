91b52510acd9cef957b8567b8b4c28bdac20e00c
Cosme Andrade <netenhoandrade@gmail.com>
2025-02-28 19:24:48 -0300

    Resolvendo conflitos para o pull request

--- app/src/main/java/projeto_recomendacao_jogos/validacoes/VerificacaoLogin.java
+++ app/src/main/java/projeto_recomendacao_jogos/validacoes/VerificacaoLogin.java
@@ -10,19 +10,17 @@ import java.sql.SQLException;
 
 public class VerificacaoLogin {
 
-
-    private BDStrategy BDStrategy;
+    private BDStrategy contexto;
 
     public VerificacaoLogin() {
-        this.BDStrategy = new BDStrategy();
-        this.BDStrategy.setStrategy(new ManipularUsuarios());
+        this.contexto = new BDStrategy();
+        this.contexto.setStrategy(new ManipularUsuarios());
     }
 
     public boolean verificarCredenciais(String email, String senha) {
         String sql = "SELECT senha FROM usuario WHERE email = ?";
 
-        try (Connection conexao = (Connection) BDStrategy.ler(null);
-
+        try (Connection conexao = (Connection) contexto.ler(null);
              PreparedStatement stmt = conexao.prepareStatement(sql)) {
 
             stmt.setString(1, email);
